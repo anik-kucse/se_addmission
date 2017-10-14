463dea280f292196d55c8846ba8a7c7015cc352b
Anisha Tasnim <tasnimanisha@gmail.com>
2017-10-15 02:11:40 +0600

    manageAllAccount

---
+++ app/views/admin/unitDetails.php
@@ -0,0 +1,90 @@
+
+<div class="container-fluid">
+    <h1 style="text-align: center">Unit details</h1>
+    <form class="row justify-content-around" style="padding-top: 10px">
+
+        <div class="col-8 form-group row">
+            <label for="fullname" class="col-2 col-form-label">University Name</label>
+            <div class="col-10">
+                <input class="form-control" type="text" readonly = "readonly">
+            </div>
+        </div>
+
+        <div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Unit Name</label>
+            <div class="col-10">
+                <input class="form-control" type="text" readonly = "readonly">
+            </div>
+        </div>
+
+        <div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Session</label>
+            <div class="col-10">
+                <input class="form-control" type="text" readonly = "readonly">
+            </div>
+        </div>
+
+        <div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Starting Time</label>
+            <div class="col-10">
+                <input class="form-control" type="date" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Starting Time</label>
+            <div class="col-10">
+                <input class="form-control" type="date" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Starting Time</label>
+            <div class="col-10">
+                <input class="form-control" type="date" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label  class="col-2 col-form-label">Ending Time</label>
+            <div class="col-10">
+                <input class="form-control" type="date" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Selection Time</label>
+            <div class="col-10">
+                <input class="form-control" type="date" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Exam Time</label>
+            <div class="col-10">
+                <input class="form-control" type="date" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Form Price</label>
+            <div class="col-10">
+                <input class="form-control" type="text" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Service Charge</label>
+            <div class="col-10">
+                <input class="form-control" type="text" readonly = "readonly">
+            </div>
+        </div>
+		
+		<div class="col-8 form-group row">
+            <label class="col-2 col-form-label">Detail</label>
+            <div class="col-10">
+                <input class="form-control" type="text" readonly = "readonly">
+            </div>
+        </div>		
+    </form>
+</div>
